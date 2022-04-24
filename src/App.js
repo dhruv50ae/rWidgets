fe05ff064b2510df830f4b27a3d4ab3dce6b5aeb
dhruv50ae <linkdp2012@gmail.com>
2022-04-24 06:57:42 +0100

    Added a search file

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from "react";
-import Accordion from "./component/Accordion";
+// import Accordion from "./component/Accordion";
+import Search from "./component/Search";
 
-const items = [
-  { title: "What is React?", content: "React is a frontend framework" },
-  {
-    title: "Why use React?",
-    content: "It is regularly maintained and optimised",
-  },
-  {
-    title: "Who uses React?",
-    content: "A ton of engineers since it's favouraited amongst them",
-  },
-];
+// const items = [
+//   { title: "What is React?", content: "React is a frontend framework" },
+//   {
+//     title: "Why use React?",
+//     content: "It is regularly maintained and optimised",
+//   },
+//   {
+//     title: "Who uses React?",
+//     content: "A ton of engineers since it's favouraited amongst them",
+//   },
+// ];
 const App = () => {
   return (
     <div>
-      <Accordion items={items} />
+      {/* <Accordion items={items} /> */}
+      <Search />
     </div>
   );
 };
