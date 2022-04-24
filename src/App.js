27686e0fc711fc36e982e32ba67d6a21077d4690
dhruv50ae <linkdp2012@gmail.com>
2022-04-24 06:23:04 +0100

    Fixed the items bug

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from "react";
 import Accordion from "./component/Accordion";
 
+const items = [
+  { title: "What is React?", content: "React is a frontend framework" },
+  {
+    title: "Why use React?",
+    content: "It is regularly maintained and optimised",
+  },
+  {
+    title: "Who uses React?",
+    content: "A ton of engineers since it's favouraited amongst them",
+  },
+];
 const App = () => {
-
-  items = [
-    {title: "What is React?",
-      content: "React is a frontend framework",},{title: "Why use React?",
-        content: "It is regularly maintained and optimised",},{title: "Who uses React?",
-          content: "A ton of engineers since it's favouraited amongst them",},
-  ]
-
   return (
     <div>
-      <Accordion />
+      <Accordion items={items} />
     </div>
   );
 };
