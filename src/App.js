a9d0791cc6ea54cd44610365f49147606251bc07
dhruv50ae <linkdp2012@gmail.com>
2022-04-24 06:16:29 +0100

    created accordion

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import Accordion from "./component/Accordion";
 
 const App = () => {
-  return <div>App</div>;
+
+  items = [
+    {title: "What is React?",
+      content: "React is a frontend framework",},{title: "Why use React?",
+        content: "It is regularly maintained and optimised",},{title: "Who uses React?",
+          content: "A ton of engineers since it's favouraited amongst them",},
+  ]
+
+  return (
+    <div>
+      <Accordion />
+    </div>
+  );
 };
 
 export default App;
