9e4ac5cc421bd074a190d62e15643deaa12bbe70
dhruv50ae <linkdp2012@gmail.com>
2022-04-24 06:43:35 +0100

    Added useState

--- src/component/Accordion.js
+++ src/component/Accordion.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Accordion = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(null);
   const onTitleClick = (index) => {
-    console.log("first", index);
+    setActiveIndex(index);
   };
   const renderedItems = items.map((item, index) => {
     return (
@@ -22,7 +23,12 @@ const Accordion = ({ items }) => {
       </React.Fragment>
     );
   });
-  return <div className="ui styled accordion">{renderedItems}</div>;
+  return (
+    <div className="ui styled accordion">
+      {renderedItems}
+      <h1>{activeIndex}</h1>
+    </div>
+  );
 };
 
 export default Accordion;
