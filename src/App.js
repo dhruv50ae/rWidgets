fe674f7a8c5edfde9c9d47ca65e083dca6b44234
dhruv50ae <linkdp2012@gmail.com>
2022-04-25 11:45:05 +0100

    Added basic routing

--- src/App.js
+++ src/App.js
@@ -1,39 +1,35 @@
-import React, { useState } from "react";
-// import Dropdown from "./component/Dropdown";
-import Translate from "./component/Translate";
-// import Accordion from "./component/Accordion";
-// import Search from "./component/Search";
+import React from "react";
+import Accordion from "./component/Accordion";
+import Search from "./component/Search";
 
-// const items = [
-//   { title: "What is React?", content: "React is a frontend framework" },
-//   {
-//     title: "Why use React?",
-//     content: "It is regularly maintained and optimised",
-//   },
-//   {
-//     title: "Who uses React?",
-//     content: "A ton of engineers since it's favouraited amongst them",
-//   },
-// ];
+const items = [
+  { title: "What is React?", content: "React is a frontend framework" },
+  {
+    title: "Why use React?",
+    content: "It is regularly maintained and optimised",
+  },
+  {
+    title: "Who uses React?",
+    content: "A ton of engineers since it's favouraited amongst them",
+  },
+];
 
-// const options = [
-//   { label: "The Color Red", value: "red" },
-//   { label: "The Color Green", value: "green" },
-//   { label: "The Color Blue", value: "blue" },
-// ];
+const showAccordion = () => {
+  if (window.location.pathname === "/") {
+    return <Accordion items={items} />;
+  }
+};
+
+const showList = () => {
+  if (window.location.pathname === "/list") {
+    return <Search />;
+  }
+};
 
 const App = () => {
-  // const [selected, setSelected] = useState(options[0]);
   return (
     <div>
-      {/* <Accordion items={items} /> */}
-      {/* <Search /> */}
-      {/* <Dropdown
-        selected={selected}
-        setSelected={setSelected}
-        options={options}
-      /> */}
-      <Translate />
+      {showAccordion()} {showList()}
     </div>
   );
 };
